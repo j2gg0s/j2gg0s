befdf5b9d9d3d83e0f0d21094d3ea5a07efe9502
j2gg0s <j2gg0s@gmail.com>
2024-05-21 22:52:05 +0800

    wips-reflect-and-interface

---
+++ examples/go-interface-reflect/main.go
@@ -0,0 +1,32 @@
+package main
+
+type Mather interface {
+	Add(a, b int32) int32
+	Sub(a, b int64) int64
+}
+
+type Inter struct{ id int32 }
+
+//go:noinline
+func (adder Inter) Add(a, b int32) int32 { return a + b }
+
+//go:noinline
+func (adder Inter) Sub(a, b int64) int64 { return a - b }
+
+type Structer struct {
+	x int32
+	y string
+}
+
+//go:noinline
+func (adder Structer) Add(a, b int32) int32 { return a + b }
+
+//go:noinline
+func (adder Structer) Sub(a, b int64) int64 { return a - b }
+
+func main() {
+	// NOTE: avoid devirtualizing
+	for _, m := range []Mather{Mather(Inter{id: 6754}), Mather(Structer{7, "8"})} {
+		m.Add(10, 32)
+	}
+}
